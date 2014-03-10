8ff27becde71b860e88db926c8fae98552eaf776
MichaelVincken <michaelvinckenbe@gmail.com>
2014-03-10 14:43:01 +0100

    Inlezen GET / HTTP/1.1

--- JavaSockets/src/be/slangenvincken/cn/labs/test/TCPClient.java
+++ JavaSockets/src/be/slangenvincken/cn/labs/test/TCPClient.java
@@ -9,15 +9,18 @@ class TCPClient {
 	public static void main(String argv[]) throws Exception {
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
 				System.in));
-		Socket clientSocket = new Socket("localhost", 6789);
+		Socket clientSocket = new Socket("173.194.45.63", 80);
 		DataOutputStream outToServer = new DataOutputStream(
 				clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
 				clientSocket.getInputStream()));
 		String sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence + '\n');
-		String modifiedSentence = inFromServer.readLine();
-		System.out.println("FROM SERVER: " + modifiedSentence);
+		outToServer.writeBytes(sentence + "\n\n");
+		
+		String senteceFromServer;
+		while((senteceFromServer = inFromServer.readLine()) != null) {
+			System.out.println(senteceFromServer);
+		}
 		clientSocket.close();
 	}
 }
