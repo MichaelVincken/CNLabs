c5096518ae5fe8c20a28c6942b5f2e9b9fcd7e9d
SimSla <simon@Simons-MacBook-Air.local>
2014-03-12 14:42:58 +0100

    Persistence!

--- JavaSockets/HTTPServer/src/Handler.java
+++ JavaSockets/HTTPServer/src/Handler.java
@@ -11,6 +11,7 @@ import java.util.regex.*;
 public class Handler implements Runnable {
 	
 	private Socket connectionSocket;
+	private boolean lastRequest = true;
 	private static Pattern cmdPattern = Pattern.compile("^(?<command>\\w*) (?<path>.*?) (?<httpVersion>HTTP/\\d\\.\\d)");
 	private static Pattern headerPattern = Pattern.compile("^(.+?):[ \\t]*(.*)");
 	private static Pattern headerPatternTail = Pattern.compile("^[ \\t]+(.+)");
@@ -29,102 +30,123 @@ public class Handler implements Runnable {
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader (connectionSocket.getInputStream()));
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 			
-			System.out.println("Received: "); 
+			boolean firstLoop = true;
 			
-			//Process first line...
-			String clientSentence = inFromClient.readLine(); 
-			System.out.println(clientSentence);
-			String[] properties = tokenizeProperties(clientSentence);
+		processRequests:
+			while(!lastRequest || firstLoop){ //TODO:loopieloop
+				
+				firstLoop = false;
 			
-			//Process headers. (Although we're only interested in the Host header for now.)
-			LinkedList<String[]> headers = collectHeaders(inFromClient);
-			
-			//Process body
-			LinkedList<String> body = collectBody(inFromClient);
-			
-			//TODO: accept chunked data requests...
-			
-			//We now start building the response...
-			String response = "";
-			
-			//HTTP/1.1 -> Process Host: header
-			String domain = WEBSITE_ROOT;
-			if(properties[2].equals("HTTP/1.1")){
-				domain = getHostAddress(headers);
-				if(domain == null){
-					return400(outToClient,properties[2]);
-					return;
-				} else {
-					//Accepting absolute URLs (small workaround)
-					if(properties[1].startsWith("http://")){
-						properties[1] = properties[1].split(domain)[1];
+				//PROCESS ONE REQUEST
+				
+				//Process first line...
+				String clientSentence = inFromClient.readLine();
+				while(clientSentence.equals("\n")){
+					clientSentence = inFromClient.readLine();
+				}
+				System.out.println();
+				System.out.println("Received request: ");
+				System.out.println(clientSentence);
+				String[] properties = tokenizeProperties(clientSentence);
+				
+				//Process headers. (Although we're only interested in the Host header for now.)
+				LinkedList<String[]> headers = collectHeaders(inFromClient);
+				
+				//Process body
+				LinkedList<String> body = collectBody(inFromClient);
+				
+				//TODO: accept chunked data requests...
+				
+				//We now start building the response...
+				String response = "";
+				
+				//HTTP/1.1 -> Process Host: header
+				String domain = WEBSITE_ROOT;
+				if(properties[2].equals("HTTP/1.1")){
+					domain = getHostAddress(headers);
+					lastRequest = isLastRequest(headers);
+					if(domain == null){
+						return400(outToClient,properties[2]);
+						continue processRequests;
+					} else {
+						//Accepting absolute URLs (small workaround)
+						if(properties[1].startsWith("http://")){
+							properties[1] = properties[1].split(domain)[1];
+						}
 					}
 				}
-			}
-			//Replacing spaces in path and fixing rel paths
-			properties[1] = properties[1].replaceAll("%20", " ");
-			if(properties[1].startsWith("./")){
-				properties[1] = properties[1].substring(1);
+				//Replacing spaces in path and fixing rel paths
+				properties[1] = properties[1].replaceAll("%20", " ");
+				if(properties[1].startsWith("./")){
+					properties[1] = properties[1].substring(1);
+				}
+				
+				//Constructing local path and log
+				Path filePath = FileSystems.getDefault().getPath(domain, properties[1]);
+				
+				//TODO:If-Modified-Since: or If-Unmodified-Since: Headers
+				
+				//Processing switch
+				switch(properties[0]){
+					case "GET":		if(Files.exists(filePath)){
+										//Valid GET response.
+										System.out.println("path exists");
+										response = properties[2] + " 200 OK\n";
+										response += getHead(filePath) + "\n\n";
+										byte[] data = processGet(filePath);
+										outToClient.writeBytes(response);
+										outToClient.write(data);
+										outToClient.writeBytes("\n\n");
+										break;					
+									} else {
+										//404 Error
+										System.out.println("lolnope");
+										return404(outToClient,properties[2]);
+										break;
+									}
+					case "HEAD":	if(Files.exists(filePath)){
+										//Valid HEAD response
+										response = properties[2] + " 200 OK\n";
+										response += getHead(filePath) + "\n\n";
+										outToClient.writeBytes(response);
+										break;
+									} else {
+										//404 Error
+										return404(outToClient,properties[2]);
+										break;
+									}
+					case "PUT": 	Files.write(filePath, body, Charset.defaultCharset());
+									if(writePutPostLog("PUT",filePath.toString(),domain+"/"+LOG_FILENAME,properties[2],body)){
+										//Valid PUT response
+										response = properties[2] + " 200 Data written\n";
+										response += getHead(filePath) + "\n\n";
+										outToClient.writeBytes(response);
+										break;
+									} else {
+										//500 Error
+										return500(outToClient,properties[2]);
+										break;
+									}
+									
+					case "POST": 	if(writePutPostLog("POST",filePath.toString(),domain+"/"+LOG_FILENAME,properties[2],body)){
+										//Valid POST response
+										response = properties[2] + " 200 Data posted\n";
+										response += getHead(filePath) + "\n\n";
+										outToClient.writeBytes(response);
+										break;
+									}else{
+										//500 error
+										return500(outToClient,properties[2]);
+										break;
+									}
+					default: 		//501 error
+									response = properties[2] + " 501 Not Implemented\n\n";
+									outToClient.writeBytes(response);
+									break;
+				}
 			}
 			
-			//Constructing local path and log
-			Path filePath = FileSystems.getDefault().getPath(domain, properties[1]);
-			
-			//TODO:If-Modified-Since: or If-Unmodified-Since: Headers
-			
-			//Processing switch
-			switch(properties[0]){
-				case "GET":		if(Files.exists(filePath)){
-									//Valid GET response.
-									System.out.println("path exists");
-									response = properties[2] + " 200 OK\n";
-									response += getHead(filePath) + "\n\n";
-									byte[] data = processGet(filePath);
-									outToClient.writeBytes(response);
-									outToClient.write(data);
-									outToClient.writeBytes("\n\n");
-									return;					
-								} else {
-									//404 Error
-									System.out.println("lolnope");
-									return404(outToClient,properties[2]);
-									return;
-								}
-				case "HEAD":	if(Files.exists(filePath)){
-									//Valid HEAD response
-									response = properties[2] + " 200 OK\n";
-									response += getHead(filePath) + "\n\n";
-									outToClient.writeBytes(response);
-									return;
-								} else {
-									//404 Error
-									return404(outToClient,properties[2]);
-									return;
-								}
-				case "PUT": 	Files.write(filePath, body, Charset.defaultCharset());
-								if(writePutPostLog("PUT",filePath.toString(),domain+"/"+LOG_FILENAME,properties[2],body)){
-									//Valid PUT response
-									response = properties[2] + " 200 Data written\n";
-									response += getHead(filePath) + "\n\n";
-									outToClient.writeBytes(response);
-									return;
-								} else {
-									//500 Error
-									return500(outToClient,properties[2]);
-								}
-								
-				case "POST": 	if(writePutPostLog("POST",filePath.toString(),domain+"/"+LOG_FILENAME,properties[2],body)){
-									//Valid POST response
-									response = properties[2] + " 200 Data posted\n";
-									response += getHead(filePath) + "\n\n";
-									outToClient.writeBytes(response);
-									return;
-								}else{
-									//500 error
-									return500(outToClient,properties[2]);
-								}
-				default: response = properties[2] + " 501 Not Implemented\n\n";
-			}
+			//connectionSocket.close();
 			
 		}catch(IOException e){
 			//TODO: ?
@@ -162,8 +184,8 @@ public class Handler implements Runnable {
 		Date now = new Date();
 		String response = "Date: " + now.toString().substring(0, 3) + " " + now.toGMTString() + "\n";
 		response += "Content-Type: " + Files.probeContentType(filePath) + "\n";
-		response += "Content-Length: " + Files.size(filePath) + "\n";
-		response += "Connection: close";
+		response += "Content-Length: " + Files.size(filePath);
+		if(lastRequest) response += "\nConnection: close";
 		
 		return response;
 	}
@@ -181,8 +203,9 @@ public class Handler implements Runnable {
 	private void return500(DataOutputStream out, String version) throws IOException{
 		String response = version + " 500 Server error\n"
 				+ "Content-Type: text/html\n"
-				+ "Content-Length: 52\n"
-				+ "\n"
+				+ "Content-Length: 52\n";
+		if(lastRequest) response += "Connection: close\n";
+		response += "\n"
 				+ "<html><body><h2>500: Server Error</h2></body></html>";
 		out.writeBytes(response);
 		
@@ -201,8 +224,9 @@ public class Handler implements Runnable {
 	private void return404(DataOutputStream out, String version) throws IOException{
 		String response = version + " 404 File not found\n"
 				+ "Content-Type: text/html\n"
-				+ "Content-Length: 56\n"
-				+ "\n"
+				+ "Content-Length: 56\n";
+				if(lastRequest) response += "Connection: close\n";
+		response += "\n"
 				+ "<html><body>\n"
 				+ "<h2>404: File not found</h2>\n"
 				+ "</body></html>\n\n";
@@ -223,8 +247,9 @@ public class Handler implements Runnable {
 	private void return400(DataOutputStream out, String version) throws IOException{
 		String response = "HTTP/1.1 400 Bad Request\n"
 				+ "Content-Type: text/html\n"
-				+ "Content-Length: 111\n"
-				+ "\n"
+				+ "Content-Length: 111\n";
+				if(lastRequest) response += "Connection: close\n";
+		response += "\n"
 				+ "<html><body>\n"
 				+ "<h2>No Host: header received</h2>\n"
 				+ "HTTP 1.1 requests must include the Host: header.\n"
@@ -262,6 +287,24 @@ public class Handler implements Runnable {
 			}
 		}
 		return null;
+	}
+	
+	/**
+	 * Used in HTTP/1.1 compliance. Checks if this is marked as the last request.
+	 * 
+	 * @param 	headers
+	 * 			Array of headers.
+	 * 			- First element contains the name of the header.
+	 * 			- Second element contains the data.
+	 * @return	true if contains Connection: close, false outherwise
+	 */
+	private boolean isLastRequest(LinkedList<String[]> headers) {
+		for(String[] header : headers){
+			if(header[0].equals("Connection") && header[1].contains("close")){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
